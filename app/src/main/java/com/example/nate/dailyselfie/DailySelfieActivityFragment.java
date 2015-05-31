76b5708d14ec79cd5bb6333d89cffa7796a1369b
Liquid5n0w <nate359@gmail.com>
2015-05-30 20:53:52 -0400

    Initial commit

---
+++ app/src/main/java/com/example/nate/dailyselfie/DailySelfieActivityFragment.java
@@ -0,0 +1,23 @@
+package com.example.nate.dailyselfie;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class DailySelfieActivityFragment extends Fragment {
+
+    public DailySelfieActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_daily_selfie, container, false);
+    }
+}
